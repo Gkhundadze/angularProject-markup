1ecdaf21a10a91a4f12ca73e4ab17ef80fafe9a8
Giga <60932604+Gkhundadze@users.noreply.github.com>
2020-12-23 03:02:02 +0400

    created terrible calculator
    but mine :D

--- src/app/components/video-section/temporaty/temporaty.component.ts
+++ src/app/components/video-section/temporaty/temporaty.component.ts
@@ -6,17 +6,39 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./temporaty.component.css']
 })
 export class TemporatyComponent implements OnInit {
-
-  public num1:number;
-  public num2:number;
-  public result:number;
+  public errorMessage:string = "Calculator Works";
+  public num1: number;
+  public num2: number;
+  public result: number;
+  public result2: number;
+  public result3: number;
+  public result4: number;
   constructor() { }
   ngOnInit(): void {
   }
-  add(num1, num2){
+  add(num1, num2) {
     let val = parseInt(num1.value);
     let val2 = parseInt(num2.value);
-    let res = val+val2;
+    let res = val + val2;
     this.result = res;
+    
+  }
+  sub(num3, num4) {
+    let val = parseInt(num3.value);
+    let val2 = parseInt(num4.value);
+    let res = val - val2;
+    this.result2 = res;
+  }
+  mult(num5, num6) {
+    let val = parseInt(num5.value);
+    let val2 = parseInt(num6.value);
+    let res = val * val2;
+    this.result3 = res;
+  }
+  div(num7, num8) {
+    let val = parseInt(num7.value);
+    let val2 = parseInt(num8.value);
+    let res = val / val2;
+    this.result4 = res;
   }
 }
