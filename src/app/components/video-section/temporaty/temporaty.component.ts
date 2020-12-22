b674bd8e6d5a810e8a3ec16eda0ccc2227260ae1
Giga <60932604+Gkhundadze@users.noreply.github.com>
2020-12-23 03:31:34 +0400

    update stupid calculator

--- src/app/components/video-section/temporaty/temporaty.component.ts
+++ src/app/components/video-section/temporaty/temporaty.component.ts
@@ -7,8 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TemporatyComponent implements OnInit {
   public errorMessage:string = "Calculator Works";
-  public num1: number;
-  public num2: number;
+  public testVatiable:string = ":D"
   public result: number;
   public result2: number;
   public result3: number;
@@ -16,26 +15,26 @@ export class TemporatyComponent implements OnInit {
   constructor() { }
   ngOnInit(): void {
   }
-  add(num1, num2) {
+  add(num1, num2): void {
     let val = parseInt(num1.value);
     let val2 = parseInt(num2.value);
     let res = val + val2;
     this.result = res;
     
   }
-  sub(num3, num4) {
+  sub(num3, num4): void {
     let val = parseInt(num3.value);
     let val2 = parseInt(num4.value);
     let res = val - val2;
     this.result2 = res;
   }
-  mult(num5, num6) {
+  mult(num5, num6): void {
     let val = parseInt(num5.value);
     let val2 = parseInt(num6.value);
     let res = val * val2;
     this.result3 = res;
   }
-  div(num7, num8) {
+  div(num7, num8): void {
     let val = parseInt(num7.value);
     let val2 = parseInt(num8.value);
     let res = val / val2;
